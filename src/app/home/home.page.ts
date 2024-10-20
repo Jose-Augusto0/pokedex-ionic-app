c5cdaca57db547648bcf8a9d236988d4b06fae73
José Augusto <joseaugustogomessantos12@gmail.com>
2024-10-20 14:43:30 -0300

    estrutura inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,25 +1,63 @@
-import { Component, inject } from '@angular/core';
-import { RefresherCustomEvent } from '@ionic/angular';
-import { MessageComponent } from '../message/message.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
-import { DataService, Message } from '../services/data.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  private data = inject(DataService);
-  constructor() {}
+export class HomePage implements OnInit{
 
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
+  pokemonsList:any = []
+
+  constructor(private service: DataService) {}
+
+  
+  ngOnInit(): void {
+    this.getPokemons()
+  }
+  getPokemons(): void {
+    const pokemonNames: string[] = [
+      'pikachu',
+      'machamp',
+      'venusaur',
+      'arbok',
+      'blastoise',
+      'golbat',
+      'arceus',
+      'lugia',
+    ];
+
+    pokemonNames.filter((name) => {
+      this.service.getPokemons(name).subscribe({
+        next: (res) => {
+
+        const pokemon: any = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types,
+            stats: res.stats,
+            moves: res.moves,
+            abilities: res.abilities,
+            height: res.height,
+            weight: res.weight,
+            species: res.species,
+            base_experience: res.base_experience,
+          };
+          this.pokemonsList.push(pokemon);
+          console.log(this.pokemonsList)
+
+        },
+        error: (err) => console.log('Não consegui encontrar o Pokémon', err),
+      });
+    });
+
+
+
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
+  
 }
