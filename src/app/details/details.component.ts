2306acb662e9d67b510f01519c48422bbc13ea2a
José Augusto <joseaugustogomessantos12@gmail.com>
2024-10-23 19:44:06 -0300

    Criacao de tabs

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-details',
@@ -8,11 +9,36 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent implements OnInit {
   pokemon: any;
+  actualTab: number = 0;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private service: DataService) {}
+
+  nextTab(tab: number) {
+    this.actualTab = tab;
+  }
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('pokeName');
-    console.log(id)
+    const name = this.route.snapshot.paramMap.get('pokeName');
+    if (name) {
+      this.service.getPokemons(name).subscribe({
+        next: (res) => {
+          const pokemon: any = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types,
+            stats: res.stats,
+            moves: res.moves,
+            abilities: res.abilities,
+            height: res.height,
+            weight: res.weight,
+            species: res.species,
+            base_experience: res.base_experience,
+          };
+          this.pokemon = pokemon;
+        },
+        error: (err) => console.log('Pokémon não encontrado', err),
+      });
+    }
   }
 }
